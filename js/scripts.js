31089165292ff3bfa3cdbc2644be99c8141b67b9
Bahay Gulle Bilgi and Misaki Koonce <bahay@>
2019-08-29 09:11:51 -0700

    Fix the bug that showing random number when dice number is 1 after hold click - kill the User.calcTurnTotal

--- js/scripts.js
+++ js/scripts.js
@@ -10,29 +10,17 @@ function User (diceNumber, scoreTotal, turnTotal, userNumber) {
 User.prototype.isOne = function(diceRoll) {
   this.diceNumber = diceRoll;
   if(this.diceNumber === 1) {
-    switchUser();
     $("input#throw-total").val("0");
+    switchUser();
   } else {
-    return this.diceNumber;
-  }
-}
-
-User.prototype.calcTurnTotal = function() {
-  if(this.diceNumber === 1) {
-    this.turnTotal = 0
-
-    //$("input#throw-total").val("");
-  } else if(this.turnTotal += this.diceNumber){
+    this.turnTotal += this.diceNumber;
     $("input#throw-total").val(this.turnTotal);
   }
 }
 
 User.prototype.sumScore = function() {
-  if(this.scoreTotal === 0) {
-    this.scoreTotal = this.turnTotal;
-  } else {
     this.scoreTotal += this.turnTotal;
-  }
+
   if(this.scoreTotal >= 100) {
     $(".modal").modal();
     $("#close").click(() => {
@@ -40,7 +28,6 @@ User.prototype.sumScore = function() {
     });
   }
 
-  console.log('score total: ', this.scoreTotal);
 }
 
 // Check through dice and update turn total, continue the user throw 1 or click hold
@@ -56,7 +43,6 @@ function throwDice () {
   var diceRoll = Math.floor( Math.random() * 6 ) +1;
   $("input#dice").val(diceRoll);
   currentUser.isOne(diceRoll);
-  currentUser.calcTurnTotal();
 }
 
 // User Interface logic
